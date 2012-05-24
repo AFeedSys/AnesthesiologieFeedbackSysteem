2e4878626c03fa95d100ff50dbf3b0056fbf93ac
Lisanne Bik <lisannebik_29@hotmail.com>
2012-05-24 14:40:13 +0200

    Eerste versie UI
    CSS en HTML

---
+++ main.php
@@ -0,0 +1,16 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
