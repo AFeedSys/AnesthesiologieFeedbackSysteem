fc271dbb0082d8e9c35bfd41bcd0265dd2547e6a
missmondt <milja.m@planet.nl>
2012-05-25 13:51:02 +0200

    php files van alles + gekloot

---
+++ totaal.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+
+<head>
+
+<link rel="stylesheet" type="text/css" href="styles/totaal.css">
+<link rel="stylesheet" type="text/css" href="styles/stijl.css">
+<script type="text/javascript" src="script.js"></script>
+
+<title>AFeedSys</title>
+
+</head>
+
+<body>
+
+<!--<div class="container">
+-->
+	<div class="header">
+		<h1>AFeedSys</h1>
+	</div>
+
+    <div class="menu"> 
+        <?php
+        include 'menu.php'
+        ?>
+	</div>
+
+	<div class="content1">
+            
+            <h2>hallo</h2>
+	</div>
+
+        <div class="clear">
+        </div>
+            
+	<div class="content2">
+            
+            <h2>hallo2</h2>
+	</div>
+
+	<div class="content3">
+            
+            <h2>hallo3</h2>
+	</div>
+
+	<div class="footer"> 
+        <?php
+        include 'footer.php'
+        ?>
+	</div>
+
+<!--</div>-->
+ 
+</body>
+</html>
