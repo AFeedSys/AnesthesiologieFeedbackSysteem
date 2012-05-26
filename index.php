4872ac0982ff1cffbdb14e9eec641b709b7352e2
Coen Meulenkamp <coenmeulenkamp@gmail.com>
2012-05-26 17:39:00 +0200

    start on the databasemanager

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
+<?php session_start();
+$_SESSION['userType'] = 'admin';?>
 <html>
     <head>
     </head>
     
     <body>
         <?php
-        echo "Hello World";
+        echo "Hello World <br />";
+        echo $_SESSION['userType'];
+        include './managers/databaseManager.php';
         ?>
     </body>
 </html>
