de27d5d6607cb6fb62db35d3a273fd8d4e9310ae
Coen Meulenkamp <coenmeulenkamp@gmail.com>
2012-06-13 13:49:16 +0200

    cherry-pick out of ShowCase

--- sql-scripts/VIEW_maandtotalen.sql
+++ sql-scripts/VIEW_maandtotalen.sql
@@ -1,4 +1,6 @@
+DROP VIEW IF EXISTS maandtotalen;
+
 CREATE VIEW maandtotalen AS
 SELECT datum, SUM(shouldTotaal) AS maandShouldTot, SUM(doneTotaal) AS maandDoneTotaal
 FROM `protocoltotalen`
-GROUP BY datum
+GROUP BY datum;
